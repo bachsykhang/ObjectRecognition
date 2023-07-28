ebd0599921f3c566bb64bdfc0b2fed7c8d7d85f2
Pham Nghia <npham2133@gmail.com>
2023-07-28 13:48:39 +0700

    Update nhan dien xe khi o khoang cach nhat dinh

--- nhandienvathela.py
+++ nhandienvathela.py
@@ -23,11 +23,13 @@ engine = pyttsx3.init()
 
 # Vị trí nguy hiểm dưới vị trí trục x (đơn vị: pixel)
 danger_zone_y = 300
-
+# Khoảng cách tối đa để phát hiện đối tượng xe
+max_distance = 150 
 # Hàm để đọc cảnh báo bằng giọng nói mà không làm dừng frame
 def speak_warning():
     # Phát ra cảnh báo bằng giọng nói
-    engine.say("wanning")
+    engine.say("Watch out for warnings")
+    engine.setProperty('volume', 4)
     engine.runAndWait()
 
 while True:
@@ -43,17 +45,20 @@ while True:
     gray = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
 
     # Sử dụng Haar Cascade Classifier để nhận diện xe cộ trong frame
-    cars = car_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5, minSize=(30, 30))
+    cars = car_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5, minSize=(40, 40))
 
     # Vẽ hình chữ nhật xung quanh các xe cộ nhận diện được
     for (x, y, w, h) in cars:
-        cv2.rectangle(resized_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Kiểm tra nếu vị trí y của khung ảnh nằm gần dưới vị trí trục x (nguy hiểm)
-        if y + h >= danger_zone_y:
-            # Sử dụng luồng riêng biệt để phát cảnh báo bằng giọng nói mà không làm dừng frame
-            warning_thread = threading.Thread(target=speak_warning)
-            warning_thread.start()
+         # Tính khoảng cách từ tọa độ của đối tượng đến trung tâm khung hình
+        distance = abs(x + w/2 - resized_frame.shape[1]/2)
+        # Chỉ vẽ đối tượng và phát cảnh báo nếu nó ở gần trung tâm khung hình
+        if distance <= max_distance:
+            cv2.rectangle(resized_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Kiểm tra nếu vị trí y của khung ảnh nằm gần dưới vị trí trục x (nguy hiểm)
+            if y + h >= danger_zone_y:
+                # Sử dụng luồng riêng biệt để phát cảnh báo bằng giọng nói mà không làm dừng frame
+                warning_thread = threading.Thread(target=speak_warning)
+                warning_thread.start()
 
     # Hiển thị frame với các hình chữ nhật được vẽ xung quanh xe cộ
     cv2.imshow('Car Detection', resized_frame)
